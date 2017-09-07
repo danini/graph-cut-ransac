f9afa30433e67f2fb9f3734dfeab04c950b21075
danini <majti89@gmail.com>
2017-09-07 12:54:45 +0200

    2

---
+++ VisionFrameWork/six_point_algorithm/sturm.cc
@@ -0,0 +1,42 @@
+// Copyright Richard Hartley, 2010
+static const char *copyright = "Copyright Richard Hartley, 2010";
+
+//--------------------------------------------------------------------------
+// LICENSE INFORMATION
+//
+// 1.  For academic/research users:
+//
+// This program is free for academic/research purpose:   you can redistribute
+// it and/or modify  it under the terms of the GNU General Public License as 
+// published by the Free Software Foundation, either version 3 of the License,
+// or (at your option) any later version.
+//
+// Under this academic/research condition,  this program is distributed in 
+// the hope that it will be useful, but WITHOUT ANY WARRANTY; without even 
+// the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+// PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along 
+// with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// 2.  For commercial OEMs, ISVs and VARs:
+// 
+// For OEMs, ISVs, and VARs who distribute/modify/use this software 
+// (binaries or source code) with their products, and do not license and 
+// distribute their source code under the GPL, please contact NICTA 
+// (www.nicta.com.au), and NICTA will provide a flexible OEM Commercial 
+// License. 
+//
+//---------------------------------------------------------------------------
+
+/*
+ * sturm.c
+ *
+ * the functions to build and evaluate the Sturm sequence
+ */
+
+// #define RH_DEBUG
+
+#include "hidden6.h"
+#include <math.h>
+#include <stdio.h>
