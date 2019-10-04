8e512dbb9e75e3e6c332cab9b11fecce828beeea
Dmytro Mishkin <ducha.aiki@gmail.com>
2019-10-04 21:17:46 +0300

    added bindings for E

--- src/pygcransac/include/gcransac_python.h
+++ src/pygcransac/include/gcransac_python.h
@@ -10,6 +10,19 @@ int findFundamentalMatrix_(std::vector<double>& srcPts,
                            double conf,
                            int max_iters);
 
+int findEssentialMatrix_(std::vector<double>& srcPts_norm,
+                           std::vector<double>& dstPts_norm,
+                           std::vector<bool>& inliers,
+                           std::vector<double>&E,
+                               std::vector<double>& src_K,
+                           std::vector<double>& dst_K,
+                   
+                           int h1, int w1, int h2, int w2,
+                           double threshold,
+                           double conf,
+                           int max_iters);
+
+
 int findHomography_(std::vector<double>& srcPts,
                     std::vector<double>& dstPts,
                     std::vector<bool>& inliers,
