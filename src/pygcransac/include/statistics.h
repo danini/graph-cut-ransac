c68a5457e7665380de7b6f525137b5b9ad07644d
danini <majti89@gmail.com>
2020-08-10 17:13:48 +0200

    Adding rejected/accepted model number to the statistics.

--- src/pygcransac/include/statistics.h
+++ src/pygcransac/include/statistics.h
@@ -45,7 +45,9 @@ namespace gcransac
 			size_t graph_cut_number,
 				local_optimization_number,
 				iteration_number,
-				neighbor_number;
+				neighbor_number,
+				rejected_models,
+				accepted_models;
 
 			std::string main_sampler_name,
 				local_optimizer_sampler_name;
@@ -60,6 +62,8 @@ namespace gcransac
 				local_optimization_number(0),
 				iteration_number(0),
 				neighbor_number(0),
+				accepted_models(0),
+				rejected_models(0),
 				score(0.0),
 				processing_time(0.0)
 			{
