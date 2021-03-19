a6f2a5ce1802f4ce3016b625e223791e5497d9d7
danini <majti89@gmail.com>
2021-03-19 14:56:55 +0100

    Replacing the minimal solver for homography fitting with a solver using Gaussian Elimination. This is slightly faster than LLT (avg. 4.2 -> 3.6 microsecs over 10000 problem instances).

--- src/pygcransac/include/solver_homography_four_point.h
+++ src/pygcransac/include/solver_homography_four_point.h
@@ -35,6 +35,7 @@
 
 #include "solver_engine.h"
 #include "homography_estimator.h"
+#include "math_utils.h"
 
 namespace gcransac
 {
@@ -80,14 +81,112 @@ namespace gcransac
 					size_t sample_number_, // The size of the sample
 					std::vector<Model> &models_, // The estimated model parameters
 					const double *weights_ = nullptr) const; // The weight for each point
+
+			protected:
+				OLGA_INLINE bool HomographyFourPointSolver::estimateNonMinimalModel(
+					const cv::Mat& data_,
+					const size_t* sample_,
+					size_t sample_number_,
+					std::vector<Model>& models_,
+					const double* weights_) const;
+
+				OLGA_INLINE bool HomographyFourPointSolver::estimateMinimalModel(
+					const cv::Mat& data_,
+					const size_t* sample_,
+					size_t sample_number_,
+					std::vector<Model>& models_,
+					const double* weights_) const;
 			};
 
-			OLGA_INLINE bool HomographyFourPointSolver::estimateModel(
+			OLGA_INLINE bool HomographyFourPointSolver::estimateMinimalModel(
 				const cv::Mat& data_,
-				const size_t *sample_,
+				const size_t* sample_,
 				size_t sample_number_,
-				std::vector<Model> &models_,
-				const double *weights_) const
+				std::vector<Model>& models_,
+				const double* weights_) const
+			{
+				constexpr size_t equation_number = 2;
+				const size_t columns = data_.cols;
+				Eigen::Matrix<double, 8, 9> coefficients;
+
+				const double* data_ptr = reinterpret_cast<double*>(data_.data);
+				size_t row_idx = 0;
+				double weight = 1.0;
+
+				for (size_t i = 0; i < sample_number_; ++i)
+				{
+					const size_t idx =
+						sample_ == nullptr ? i : sample_[i];
+
+					const double* point_ptr =
+						data_ptr + idx * columns;
+
+					const double
+						& x1 = point_ptr[0],
+						& y1 = point_ptr[1],
+						& x2 = point_ptr[2],
+						& y2 = point_ptr[3];
+
+					if (weights_ != nullptr)
+						weight = weights_[idx];
+
+					const double
+						minus_weight_times_x1 = -weight * x1,
+						minus_weight_times_y1 = -weight * y1,
+						weight_times_x2 = weight * x2,
+						weight_times_y2 = weight * y2;
+
+					coefficients(row_idx, 0) = minus_weight_times_x1;
+					coefficients(row_idx, 1) = minus_weight_times_y1;
+					coefficients(row_idx, 2) = -weight;
+					coefficients(row_idx, 3) = 0;
+					coefficients(row_idx, 4) = 0;
+					coefficients(row_idx, 5) = 0;
+					coefficients(row_idx, 6) = weight_times_x2 * x1;
+					coefficients(row_idx, 7) = weight_times_x2 * y1;
+					coefficients(row_idx, 8) = -weight_times_x2;
+
+					++row_idx;
+
+					coefficients(row_idx, 0) = 0;
+					coefficients(row_idx, 1) = 0;
+					coefficients(row_idx, 2) = 0;
+					coefficients(row_idx, 3) = minus_weight_times_x1;
+					coefficients(row_idx, 4) = minus_weight_times_y1;
+					coefficients(row_idx, 5) = -weight;
+					coefficients(row_idx, 6) = weight_times_y2 * x1;
+					coefficients(row_idx, 7) = weight_times_y2 * y1;
+					coefficients(row_idx, 8) = -weight_times_y2;
+					++row_idx;
+				}
+
+				Eigen::Matrix<double, 8, 1> h;
+
+				// Applying Gaussian Elimination to recover the null-space.
+				// Average time over 100000 problem instances  
+				// LLT solver (i.e., the fastest one in the Eigen library) = 4.2 microseconds
+				// Gaussian Elimination = 3.6 microseconds
+				gcransac::utils::gaussElimination<8>(
+					coefficients,
+					h);
+
+				if (h.hasNaN())
+					return false;
+
+				Homography model;
+				model.descriptor << h(0), h(1), h(2),
+					h(3), h(4), h(5),
+					h(6), h(7), 1.0;
+				models_.emplace_back(model);
+				return true;
+			}
+
+			OLGA_INLINE bool HomographyFourPointSolver::estimateNonMinimalModel(
+				const cv::Mat& data_,
+				const size_t* sample_,
+				size_t sample_number_,
+				std::vector<Model>& models_,
+				const double* weights_) const
 			{
 				constexpr size_t equation_number = 2;
 				const size_t columns = data_.cols;
@@ -95,23 +194,23 @@ namespace gcransac
 				Eigen::MatrixXd coefficients(row_number, 8);
 				Eigen::MatrixXd inhomogeneous(row_number, 1);
 
-				const double *data_ptr = reinterpret_cast<double *>(data_.data);
+				const double* data_ptr = reinterpret_cast<double*>(data_.data);
 				size_t row_idx = 0;
 				double weight = 1.0;
 
 				for (size_t i = 0; i < sample_number_; ++i)
 				{
-					const size_t idx = 
+					const size_t idx =
 						sample_ == nullptr ? i : sample_[i];
 
-					const double *point_ptr = 
+					const double* point_ptr =
 						data_ptr + idx * columns;
 
 					const double
-						&x1 = point_ptr[0],
-						&y1 = point_ptr[1],
-						&x2 = point_ptr[2],
-						&y2 = point_ptr[3];
+						& x1 = point_ptr[0],
+						& y1 = point_ptr[1],
+						& x2 = point_ptr[2],
+						& y2 = point_ptr[3];
 
 					if (weights_ != nullptr)
 						weight = weights_[idx];
@@ -145,17 +244,7 @@ namespace gcransac
 					++row_idx;
 				}
 
-				Eigen::Matrix<double, 8, 1> h;
-
-				// If we have a minimal sample, it is usually enough to solve the problem with not necessarily
-				// the most accurate solver. Therefore, we use normal equations for this
-				if (sample_number_ == sampleSize())
-				{
-					const Eigen::Matrix<double, 8, 8> coefficientsTransposed =
-						coefficients.transpose();
-					h = (coefficientsTransposed * coefficients).llt().solve(coefficientsTransposed * inhomogeneous);
-				} 
-				else // Otherwise, we want the results to be very accurate.
+				Eigen::Matrix<double, 8, 1> 
 					h = coefficients.colPivHouseholderQr().solve(inhomogeneous);
 
 				Homography model;
@@ -164,6 +253,28 @@ namespace gcransac
 					h(6), h(7), 1.0;
 				models_.emplace_back(model);
 				return true;
+			}
+
+			OLGA_INLINE bool HomographyFourPointSolver::estimateModel(
+				const cv::Mat& data_,
+				const size_t *sample_,
+				size_t sample_number_,
+				std::vector<Model> &models_,
+				const double *weights_) const
+			{				
+				// If we have a minimal sample, it is usually enough to solve the problem with not necessarily
+				// the most accurate solver. Therefore, we use normal equations for this
+				if (sample_number_ == sampleSize())
+					return estimateMinimalModel(data_,
+						sample_,
+						sample_number_,
+						models_,
+						weights_);
+				return estimateNonMinimalModel(data_,
+					sample_,
+					sample_number_,
+					models_,
+					weights_);
 			}
 		}
 	}
